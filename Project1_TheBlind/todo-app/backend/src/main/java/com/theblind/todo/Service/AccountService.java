c8693f3b78ceb92b747263d2648e9a257bd7629e
jamaalkw <jamaalwairegi@gmail.com>
2026-06-15 23:05:18 -0400

    Created validation for username

--- Project1_TheBlind/todo-app/backend/src/main/java/com/theblind/todo/Service/AccountService.java
+++ Project1_TheBlind/todo-app/backend/src/main/java/com/theblind/todo/Service/AccountService.java
@@ -11,8 +11,9 @@ import java.util.*;
 public class AccountService {
     private final AccountRepo accountRepository;
 
-    /* Constructor for AccountService
-    *
+    /* 
+    * Constructor for AccountService
+    * 
     * @param accountRepository - repository to manage Account entities
     */
     @Autowired
@@ -20,16 +21,43 @@ public class AccountService {
         this.accountRepository = accountRepository;
     }
 
-    /*  createAccount - Creates a new account with the given username and password
+    /**
+     * usernameValidater - this method checks whether a given username is valid
+     * 
+     * @param username
+     * @return true if username is not blank, does not contain whitespace, 
+     *         and is between 5 and 20 characters long (inclusive)
+     */
+    private boolean usernameValidater(String username) {
+        if (username.length() < 5 || username.length() > 20) return false;
 
+        for (char ch : username.toCharArray()) {
+            if (Character.isWhitespace(ch)) return false;
+        }
+
+        return true;
+    }
+
+    /*  
+    * createAccount - Creates a new account with the given username and password
+    *
     * @param username - the username for the new account
     * @param password - the password for the new account
     * @return the newly created Account object
 
      */
     public User createAccount(String username, String password) {
-        User newAccount = new User(username, password);
-        accountRepository.save(newAccount);
+        User newAccount = null;
+        Optional<User> existingUser = accountRepository.findByUsername(username);
+
+        if (existingUser.isPresent()) return null;
+
+        if (usernameValidater(username)) {
+            newAccount = new User(username, password);
+            accountRepository.save(newAccount);
+        }
+
+        if (newAccount == null) return null;
 
         return newAccount;
     }

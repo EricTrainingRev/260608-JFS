cd3f715f1172064078a344047a19b0479d94ca89
quiet <q@quiet.games>
2026-06-16 12:14:43 -0400

    Add query to recursively delete children

--- Project1_TheBlind/todo-app/backend/src/main/java/com/theblind/todo/Repo/TaskRepo.java
+++ Project1_TheBlind/todo-app/backend/src/main/java/com/theblind/todo/Repo/TaskRepo.java
@@ -1,14 +1,34 @@
 package com.theblind.todo.Repo;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.theblind.todo.Entity.Task;
 
 @Repository
-public interface TaskRepo extends JpaRepository<Task, Integer> {
+public interface TaskRepo extends JpaRepository<Task, UUID> {
     List<Task> findByUserId(Integer userId);
     List<Task> findByParentTaskId(Integer parentTaskId);
+
+    @Query(
+        nativeQuery = true,
+        value = """
+                WITH RECURSIVE Children AS (
+                    SELECT id FROM tasks WHERE id = :id
+
+                    UNION ALL
+
+                    SELECT child.id
+                    FROM tasks child
+                    INNER JOIN Children parent ON child.parent_task_id = parent.id
+                )
+                
+                DELETE FROM Children
+                """
+    )
+    void deleteChildren(UUID id);
 }

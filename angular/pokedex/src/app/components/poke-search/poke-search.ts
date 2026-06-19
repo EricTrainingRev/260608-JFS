c439dbb8782746b7b0a7b2bd2990cadbd309f702
EricTrainingRev <eric.suminski@revature.com>
2026-06-19 11:00:49 -0400

    adding poke-search integration with poke service

--- angular/pokedex/src/app/components/poke-search/poke-search.ts
+++ angular/pokedex/src/app/components/poke-search/poke-search.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { PokeService } from '../../services/poke-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-poke-search',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './poke-search.html',
   styleUrl: './poke-search.css',
 })
-export class PokeSearch {}
+export class PokeSearch {
+
+  pokemonIdentifier = '';
+
+  private pokeService = inject(PokeService);
+
+  searchForPokemon(){
+    this.pokeService.queryPokemon(this.pokemonIdentifier);
+  }
+
+}

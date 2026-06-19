be1626248ce6d86237cb9815c7b33e4a6c82da01
EricTrainingRev <eric.suminski@revature.com>
2026-06-19 10:00:16 -0400

    adding intitial pokeservice code

--- angular/pokedex/src/app/services/poke-service.ts
+++ angular/pokedex/src/app/services/poke-service.ts
@@ -1,6 +1,23 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+
+/*
+  This service will serve two purposes for us:
+  1. It will hold the code to actually query the PokeAPI and retrieve pokemon
+     information for us
+  2. It will store the pokemon information retrieved and provide a central
+     location for all components to access the pokemon data that need it
+*/
 
 @Injectable({
   providedIn: 'root',
 })
-export class PokeService {}
+export class PokeService {
+
+  private httpClient = inject(HttpClient)
+
+  queryPokemon(identifier: string){
+    
+  }
+
+}

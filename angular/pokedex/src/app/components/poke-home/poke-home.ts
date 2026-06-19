89ed36b0f8d2cb9ae287fb6602cea8fb9b978961
EricTrainingRev <eric.suminski@revature.com>
2026-06-19 09:54:27 -0400

    getting homepage ready to recieve pokemon data

--- angular/pokedex/src/app/components/poke-home/poke-home.ts
+++ angular/pokedex/src/app/components/poke-home/poke-home.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { PokeSearch } from "../poke-search/poke-search";
+import { PokeName } from "../poke-name/poke-name";
+import { PokeSprite } from "../poke-sprite/poke-sprite";
 
 @Component({
   selector: 'app-poke-home',
-  imports: [],
+  imports: [PokeSearch, PokeName, PokeSprite],
   templateUrl: './poke-home.html',
   styleUrl: './poke-home.css',
 })
--- angular/pokedex/src/app/interfaces/pokemon.ts
+++ angular/pokedex/src/app/interfaces/pokemon.ts
@@ -1,1 +1,16 @@
-export interface Pokemon {}
+/*
+    We know that the PokeAPI returns a significant ammount of information about
+    any pokemon queried. For our purposes we only want a bit of that data, the
+    name of the pokemon and some of its sprites. We can setup our Pokemon
+    interface to store only the data that we want from the PokeAPI, which means
+    when we go to query the data from the api we don't have to manually extract
+    these specific fields from the response body: the http client can be provided
+    this interface and extract the data for us
+*/
+export interface Pokemon {
+    name: string,
+    sprites: {
+        back_default: string,
+        front_default: string
+    }
+}

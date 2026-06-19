c963b0f105f7b192fb5c35986f1e1bbdd9c619c3
EricTrainingRev <eric.suminski@revature.com>
2026-06-19 09:43:33 -0400

    adding initial routing for pokedex

--- angular/pokedex/src/app/app.routes.ts
+++ angular/pokedex/src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { PokeHome } from './components/poke-home/poke-home';
 
-export const routes: Routes = [];
+/*
+    This is where we control what routes are available in our application. This
+    gets read by the rouer service and gives our application the ability to 
+    change the view to accomodate whatever routes need to be viewed
+*/
+
+export const routes: Routes = [
+    {
+        /*
+            This is a minimum Route object: you want to specify at least
+            the path for the route and the component to be rendered when
+            the route is utilized
+        */
+        path: 'home',
+        component: PokeHome
+    },
+    {
+        /*
+            This route object provides instructions for Angular to redirect a
+            user to the home route when they visit the webpage. Note the pathMatch
+            property: without this ANY route we try to visit in our app would
+            redirect to the home route
+        */
+        path:'',
+        redirectTo:'home',
+        pathMatch:'full'
+    }
+];

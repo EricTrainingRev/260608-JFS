be236d569347eeaa8b86a07fda6a2c1110729359
EricTrainingRev <eric.suminski@revature.com>
2026-06-30 09:55:15 -0400

    adding example test

--- project-example/demo/src/test/java/com/example/demo/rest/RestAssuredDemo.java
+++ project-example/demo/src/test/java/com/example/demo/rest/RestAssuredDemo.java
@@ -5,8 +5,17 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.TestPropertySource;
+
+import com.example.demo.entity.User;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 
 /*
     In order to get our test class configured properly we need to add a few configurations
@@ -21,4 +30,43 @@ import org.springframework.test.context.TestPropertySource;
 @TestPropertySource(locations = "classpath:test.properties")
 public class RestAssuredDemo {
     
+  // Make sure you save the random port the web server is running on in a variable
+  @LocalServerPort
+  private int port;
+
+  @BeforeEach
+  void setup(){
+    // we can specify where we want all of our requests to be sent
+    RestAssured.baseURI = "http://localhost";
+    // we can specify the port for the requests
+    RestAssured.port = port;
+  }
+
+  @Test
+  void simpleRestAssuredTest(){
+
+    // test data
+    User testUser = new User();
+    testUser.setUsername("Username");
+    testUser.setPassword("P0ssword");
+
+    /*
+      Rest Assured tests have a three part struture that follows a given/when/then pattern.
+      - given() = this method gives us the tools to put together our http request
+      - when() = this method gives us access to the tools to actually make the request
+      - then() = this method gives us access to the response and lets us validate it
+    */
+
+    // the given() let's us set up the headers, body, cookies, any sort of pre-conditions needed for us to make the request
+    given()
+      .contentType(ContentType.JSON)
+      .body(testUser)
+    // the when() is where we decide what type of request we are actually making and the endpoint we are hitting
+    .when()
+      .post("/register")
+    // the then() is where we perform our validation
+    .then()
+      .statusCode(201);
+  }
+
 }
